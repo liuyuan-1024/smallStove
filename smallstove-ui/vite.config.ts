ebc4c1cc3681be3fe4b4f342b84640c0fb192bda
liuyuan-1024 <1922381022@qq.com>
2023-04-18 21:44:40 +0800

    refactor: 重构了代码格式

--- smallstove-ui/vite.config.ts
+++ smallstove-ui/vite.config.ts
@@ -1,29 +1,29 @@
 // noinspection JSUnusedGlobalSymbols
 
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {resolve} from 'path'
+import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-        }),
-        Components({
-            resolvers: [ElementPlusResolver()],
-        }),
-    ],
-    server: {
-        port: 3333
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
+    })
+  ],
+  server: {
+    port: 3333
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 路径别名
     },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, 'src'), // 路径别名
-        },
-        extensions: ['.ts'], // 使用路径别名时想要省略的后缀名，可以自己 增减
-    },
+    extensions: ['.ts'] // 使用路径别名时想要省略的后缀名，可以自己 增减
+  }
 })
